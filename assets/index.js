38753c980221cb88774ea75444124114173c6b30
Justin Carly <carling@nodes.exchange>
2018-07-23 23:04:47 -0500

    Scaffolding out website

---
+++ assets/index.js
@@ -0,0 +1,34 @@
+import 'animate.css';
+
+import './sass/styles.scss';
+
+import Particles from 'particles.js';
+
+import Typed from 'typed.js';
+
+
+/*
+Configure Typed JS
+ */
+const typed = new Typed('#typedContent', {
+	strings: ["Masternodes", "Crypto Block Pay", "Some Other String", "Another String to type"],
+	typeSpeed: 80,
+	backSpeed: 80,
+	loop: true,
+	backDelay: 1000,
+	showCursor: true
+});
+typed.start();
+/*
+End Typed JS!
+ */
+
+
+
+
+
+
+
+
+
+
